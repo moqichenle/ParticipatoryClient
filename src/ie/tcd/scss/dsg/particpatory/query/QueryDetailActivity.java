1cbe4e108c2f182c7b907ebfcf733eae19f1f575
Lele <lihe@tcd.ie>
2013-07-15 14:57:48 +0100

    Query part: check detailed query

--- src/ie/tcd/scss/dsg/particpatory/query/QueryDetailActivity.java
+++ src/ie/tcd/scss/dsg/particpatory/query/QueryDetailActivity.java
@@ -1,5 +1,140 @@
 package ie.tcd.scss.dsg.particpatory.query;
 
-public class QueryDetailActivity {
+import ie.tcd.scss.dsg.particpatory.AppContext;
+import ie.tcd.scss.dsg.particpatory.R;
+import ie.tcd.scss.dsg.particpatory.SampleListFragment;
+import ie.tcd.scss.dsg.particpatory.util.Constant;
+import ie.tcd.scss.dsg.po.Query;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+
+public class QueryDetailActivity extends SlidingFragmentActivity {
+	private static final String TAG = "DetailedReport";
+	private AppContext context;
+	private ListFragment mFrag;
+	private String results;
+	private TextView query_content;
+	private TextView query_time;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Log.d(TAG, "loaded");
+		if (android.os.Build.VERSION.SDK_INT > 9) {
+			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+					.permitAll().build();
+			StrictMode.setThreadPolicy(policy);
+		}
+		context = (AppContext) getApplicationContext();
+		String queryId = context.getQueryId();
+		new QueryDetailTask().execute(Constant.url + "/querydetail", queryId);
+
+		setContentView(R.layout.activity_query_detail);
+		query_content = (TextView) findViewById(R.id.q_1);
+		query_time = (TextView) findViewById(R.id.q_2);
+		setupSlidingMenu(savedInstanceState);
+		
+	}
+
+	private void setupSlidingMenu(Bundle savedInstanceState) {
+
+		setBehindContentView(R.layout.menu_frame);
+		if (savedInstanceState == null) {
+			FragmentTransaction t = this.getSupportFragmentManager()
+					.beginTransaction();
+			mFrag = new SampleListFragment();
+			t.replace(R.id.menu_frame, mFrag);
+			t.commit();
+		} else {
+			mFrag = (ListFragment) this.getSupportFragmentManager()
+					.findFragmentById(R.id.menu_frame);
+		}
+
+		// customize the SlidingMenu
+		SlidingMenu sm = getSlidingMenu();
+		sm.setShadowWidthRes(R.dimen.shadow_width);
+		sm.setShadowDrawable(R.drawable.shadow);
+		sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+		sm.setFadeDegree(0.35f);
+		sm.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+
+		// getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	private class QueryDetailTask extends AsyncTask<String, Void, HttpResponse> {
+		@Override
+		protected HttpResponse doInBackground(String... urls) {
+			String url = urls[0];
+			ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+			HttpPost request = new HttpPost(url);
+			HttpClient httpClient = new DefaultHttpClient();
+			HttpResponse response = null;
+			postParameters.add(new BasicNameValuePair("queryId", urls[1]));
+
+			try {
+				request.setEntity(new UrlEncodedFormEntity(postParameters));
+				response = httpClient.execute(request);
+				if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+					Log.d(TAG, "request successfully");
+				} else {
+					Log.d(TAG, "failed");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return response;
+		}
+
+		@SuppressLint("SimpleDateFormat")
+		@Override
+		protected void onPostExecute(HttpResponse response) {
+			if (response != null) {
+				try {
+					BufferedReader reader = new BufferedReader(
+							new InputStreamReader(response.getEntity()
+									.getContent(), "UTF-8"));
+					results = reader.readLine();
+					Gson gson = new Gson();
+					Query query = new Query();
+					query = gson.fromJson(results, Query.class);
+					query_content.setText(query.getContent());
+					Date myDate = new Date(query.getQueryTime());
+					String reportDate = new SimpleDateFormat("yyyy-MM-dd HH:mm")
+							.format(myDate);
+					query_time.setText(reportDate);
+				} catch (ParseException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
